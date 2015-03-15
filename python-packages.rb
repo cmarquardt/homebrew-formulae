73c2c5b77abedd9b25ea78403d8b7d274e910a76
Christian Marquardt <christian@marquardt.sc>
2015-03-15 16:32:40 +0100

    Bumped version number; updates in the list of python packages.

--- python-packages.rb
+++ python-packages.rb
@@ -4,7 +4,7 @@ require "formula"
 
 class PythonPackages < Formula
   url "https://github.com/cmarquardt/Python-packages.git"
-  version "0.6"
+  version "0.7"
   homepage ""
 
   depends_on :python
