e8832779c5f3e321154065b60e01d3278def73b4
Christian Marquardt <christian@marquardt.sc>
2015-11-06 23:40:57 +0100

    teqc: Updated version to new release.

--- teqc.rb
+++ teqc.rb
@@ -3,9 +3,9 @@ require "formula"
 class Teqc < Formula
   desc "TEQC - The Toolkit for GPS/GLONASS/Galileo/SBAS/Beidou/QZSS Data"
   homepage "https://www.unavco.org/software/data-processing/teqc/teqc.html"
-  url "https://www.unavco.org/software/data-processing/teqc/development/teqc_OSX_i5_gcc4.3d_64.zip"
-  version "2015.06.23"
-  sha1 "19f621178adf464689cc4e83ebdb1548c1fde8b5"
+  url "http://www.unavco.org/software/data-processing/teqc/development/teqc_OSX_i5_gcc4.3d_64.zip"
+  version "2015.11.06"
+  sha1 "647ea11eb30ead524fd6b6f0c04e153f28dd9d5c"
 
   def install
     bin.install "teqc"
