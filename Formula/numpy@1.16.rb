2ded3a73b25a3ef5dbf7f8e9907eeb7e0da72d16
Christian Marquardt <christian@marquardt.sc>
2020-03-03 18:38:05 +0100

    Numpy for Python@2 - latest version: v1.16.6.

---
+++ Formula/numpy@1.16.rb
@@ -0,0 +1,80 @@
+class NumpyAT116 < Formula
+  desc "Package for scientific computing with Python"
+  homepage "https://www.numpy.org/"
+  url "https://files.pythonhosted.org/packages/b7/6f/24647f014eef9b67a24adfcbcd4f4928349b4a0f8393b3d7fe648d4d2de3/numpy-1.16.6.zip"
+  sha256 "e5cf3fdf13401885e8eea8170624ec96225e2174eb0c611c6f26dd33b489e3ff"
+  revision 1
+  head "https://github.com/numpy/numpy.git"
+
+  depends_on "gcc" => :build # for gfortran
+  depends_on "openblas"
+  #depends_on "python"
+  depends_on "python@2"
+
+  resource "Cython" do
+    url "https://files.pythonhosted.org/packages/d9/82/d01e767abb9c4a5c07a6a1e6f4d5a8dfce7369318d31f48a52374094372e/Cython-0.29.15.tar.gz"
+    sha256 "60d859e1efa5cc80436d58aecd3718ff2e74b987db0518376046adedba97ac30"
+  end
+
+  resource "nose" do
+    url "https://files.pythonhosted.org/packages/58/a5/0dc93c3ec33f4e281849523a5a913fa1eea9a3068acfa754d44d88107a44/nose-1.3.7.tar.gz"
+    sha256 "f1bffef9cbc82628f6e7d7b40d7e255aefaa1adb6a1b1d26c69a8b79e6208a98"
+  end
+
+  def install
+    openblas = Formula["openblas"].opt_prefix
+    ENV["ATLAS"] = "None" # avoid linking against Accelerate.framework
+    ENV["BLAS"] = ENV["LAPACK"] = "#{openblas}/lib/libopenblas.dylib"
+
+    config = <<~EOS
+      [openblas]
+      libraries = openblas
+      library_dirs = #{openblas}/lib
+      include_dirs = #{openblas}/include
+    EOS
+
+    Pathname("site.cfg").write config
+
+    ["python2"].each do |python|
+      version = Language::Python.major_minor_version python
+      dest_path = lib/"python#{version}/site-packages"
+      dest_path.mkpath
+
+      nose_path = libexec/"nose/lib/python#{version}/site-packages"
+      resource("nose").stage do
+        system python, *Language::Python.setup_install_args(libexec/"nose")
+        (dest_path/"homebrew-numpy-nose.pth").write "#{nose_path}\n"
+      end
+
+      ENV.prepend_create_path "PYTHONPATH", buildpath/"tools/lib/python#{version}/site-packages"
+      resource("Cython").stage do
+        system python, *Language::Python.setup_install_args(buildpath/"tools")
+      end
+
+      system python, "setup.py",
+        "build", "--fcompiler=gnu95", "--parallel=#{ENV.make_jobs}",
+        "install", "--prefix=#{prefix}",
+        "--single-version-externally-managed", "--record=installed.txt"
+    end
+  end
+
+  def caveats
+    homebrew_site_packages = Language::Python.homebrew_site_packages
+    user_site_packages = Language::Python.user_site_packages "python"
+    <<~EOS
+      This formula provides the latest version of numpy (v1.16.6) still supporting
+      Python@2, and only installs it into the Python 2.7 site-library.
+    EOS
+  end
+
+  test do
+    ["python2"].each do |python|
+      system python, "-c", <<~EOS
+        import numpy as np
+        t = np.ones((3,3), int)
+        assert t.sum() == 9
+        assert np.dot(t, t).sum() == 27
+      EOS
+    end
+  end
+end
