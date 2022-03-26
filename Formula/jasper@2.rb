6f70cbb43fb5f03b9cd8f38b061b7b26c0c4d548
Christian Marquardt <christian@marquardt.sc>
2022-03-26 13:02:02 +0100

    Added an older version of Jasper (keg-only) for the old version of eccodes.

---
+++ Formula/jasper@2.rb
@@ -0,0 +1,55 @@
+class JasperAT2 < Formula
+  desc "Library for manipulating JPEG-2000 images"
+  homepage "https://ece.engr.uvic.ca/~frodo/jasper/"
+  url "https://github.com/jasper-software/jasper/releases/download/version-2.0.33/jasper-2.0.33.tar.gz"
+  sha256 "28d28290cc2eaf70c8756d391ed8bcc8ab809a895b9a67ea6e89da23a611801a"
+  license "JasPer-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^version[._-]v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  keg_only "this is legacy version of Jasper only used for an old (2.16) version of ecCodes"
+
+  depends_on "cmake" => :build
+  depends_on "jpeg"
+
+  on_linux do
+    depends_on "freeglut"
+  end
+
+  def install
+    mkdir "build" do
+      args = std_cmake_args
+      args << "-DJAS_ENABLE_DOC=OFF"
+
+      if OS.mac?
+        # Make sure macOS's GLUT.framework is used, not XQuartz or freeglut
+        # Reported to CMake upstream 4 Apr 2016 https://gitlab.kitware.com/cmake/cmake/issues/16045
+        glut_lib = "#{MacOS.sdk_path}/System/Library/Frameworks/GLUT.framework"
+        args << "-DGLUT_glut_LIBRARY=#{glut_lib}"
+      end
+
+      system "cmake", "..",
+        "-DJAS_ENABLE_AUTOMATIC_DEPENDENCIES=false",
+        "-DJAS_ENABLE_SHARED=ON",
+        *args
+      system "make"
+      system "make", "install"
+      system "make", "clean"
+
+      system "cmake", "..",
+        "-DJAS_ENABLE_SHARED=OFF",
+        *args
+      system "make"
+      lib.install "src/libjasper/libjasper.a"
+    end
+  end
+
+  test do
+    system bin/"jasper", "--input", test_fixtures("test.jpg"),
+                         "--output", "test.bmp"
+    assert_predicate testpath/"test.bmp", :exist?
+  end
+end
